f3b607268577014c8521c430d8931a4b9eb55e3c
Ronaldo Sierra <sierraronaldo81@gmail.com>
2026-08-26 21:44:10 -0500

    feat: build frontend assets and initialize application bootstrap configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             \App\Http\Middleware\SecurityHeaders::class,
         ]);
